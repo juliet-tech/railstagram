b08fc855379d6b3d06d7c8fda5901c4e42ba4ed0
Juliettech <juliettech.1393@gmail.com>
2019-05-08 15:24:41 -0500

    created authenticable module to export all methods from overriding devise login to allow users to be able to sign in with both their username and their emails

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,6 +3,8 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  # We added this to allow users to be able to sign in with username and password
+  config.authentication_keys = [:login]
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
@@ -296,4 +298,6 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+
 end
